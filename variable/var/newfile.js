0eea860343f6990d437b4ddc1a172fe2d30ec864
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2026-01-12 19:54:59 +0530

    var
    Add newfile.js demonstrating variable scopes and function behavior.

---
+++ variable/var/newfile.js
@@ -0,0 +1,18 @@
+//block
+{
+    var a=10;
+}
+console.log(a);
+//function 
+function g1(){
+  var=30;
+  console.log(a);
+}
+g1();
+//global
+var a;
+function g(){
+    a=50;
+}
+g();
+console.log(a);
