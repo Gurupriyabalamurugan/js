730828aee3be0091ccce6fa95b29348b4e03220a
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2026-01-14 13:49:15 +0530

    datatypes.js

---
+++ datatypes/datatypes.js
@@ -0,0 +1,16 @@
+const a1=20;
+console.log(a1);
+const bigint=12345678890385390134225346n;
+console.log(bigint);
+
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+console.log(phrase);
+console.log(typeof a1);
+
+let mynewvariable;
+console.log(mynewvariable);
+
+let height=null;
+console.log(height); 
