d332045f333fc0831af1f4e0634dd6d3db36ea44
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2026-01-14 13:52:35 +0530

    change in datatype.js
    Updated string variable definitions and added symbol examples.

--- datatypes/datatypes.js
+++ datatypes/datatypes.js
@@ -1,11 +1,11 @@
 const a1=20;
 console.log(a1);
-const bigint=12345678890385390134225346n;
+const bigint=12345678890385390134225346n; //ends with n
 console.log(bigint);
 
 let str = "Hello";
-let str2 = 'Single quotes are ok too';
-let phrase = `can embed another ${str}`;
+let str2 = 'Single quotes';
+let phrase = `can embed another string ${str}`;
 console.log(phrase);
 console.log(typeof a1);
 
@@ -14,3 +14,7 @@ console.log(mynewvariable);
 
 let height=null;
 console.log(height); 
+
+let symbol1 = Symbol("Geeks")
+let symbol2 = Symbol("Geeks")
+console.log(symbol1 == symbol2);//unique identifier
