3e43458128143e91dfa6f8bc0920bac29928e02f
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2026-01-16 11:37:05 +0530

    switch statement

---
+++ switch/switchstatement.js
@@ -0,0 +1,25 @@
+let choice=1;
+switch(choice){
+    case 1:
+        console.log("1 is Monday");
+        break;
+    case 2:
+    case 3://grouping of cases 2 and 3
+        console.log("2 is Tuesday or 3 is Wednesday");
+        break;
+    case 4:
+        console.log("4 is Thursday");
+        break;
+    case 5:
+        console.log("5 is Friday");
+        break;
+    case 6:
+        console.log("6 is Saturday");
+        break;
+    case 7:
+        console.log("7 is Sunday");
+        break;
+    default:
+        console.log("ENter vaild input");        
+        
+}
